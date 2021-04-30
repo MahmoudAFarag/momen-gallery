4abb4d2bc8e1471d5d2fbbac846b32d07f0a5885
Mahmoud Ashraf <mahmoudashrafsaad8@gmail.com>
2021-04-30 14:31:17 +0200

    fix fonts

--- components/Featured/index.js
+++ components/Featured/index.js
@@ -6,30 +6,35 @@ export default function index() {
     <>
       <Header />
       <SimpleGrid
-        minChildWidth={{ base: "250px", md: "320px", lg: "460px" }}
-        spacing={{ base: "60px", md: "50px", lg: "40px" }}
+        minChildWidth={{
+          base: "250px",
+          md: "320px",
+          lg: "460px",
+          "2xl": "600px",
+        }}
+        spacing={{ base: "60px", md: "50px", lg: "40px", "2xl": "60px" }}
         my="2rem"
         mx="2rem"
       >
         <Box
           data-aos="zoom-in-up"
           bg="tomato"
-          height={{ base: "150", md: "180", lg: "300px" }}
+          height={{ base: "150", md: "180", lg: "300px", "2xl": "400px" }}
         ></Box>
         <Box
           data-aos="zoom-in-up"
           bg="tomato"
-          height={{ base: "150", md: "180", lg: "300px" }}
+          height={{ base: "150", md: "180", lg: "300px", "2xl": "400px" }}
         ></Box>
         <Box
           data-aos="zoom-in-up"
           bg="tomato"
-          height={{ base: "150", md: "180", lg: "300px" }}
+          height={{ base: "150", md: "180", lg: "300px", "2xl": "400px" }}
         ></Box>
         <Box
           data-aos="zoom-in-up"
           bg="tomato"
-          height={{ base: "150", md: "180", lg: "300px" }}
+          height={{ base: "150", md: "180", lg: "300px", "2xl": "400px" }}
         ></Box>
       </SimpleGrid>
     </>
